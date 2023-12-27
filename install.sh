af488278ef6a8ca2316cb071c5a5afd25515112e
SantaSpeen <santaspeen@gmail.com>
2023-12-28 00:53:05 +0300

    added some checks

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+setup_color() {
+  if ! [ -t 1 ]; then
+    FMT_RED=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+setup_color
+
+user_can_sudo() {
+  command_exists sudo || return 1
+  case "$PREFIX" in
+  *com.termux*) return 1 ;;
+  esac
+  # shellcheck disable=SC1007
+  ! LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"
+}
+
 command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
@@ -21,10 +45,15 @@ command_exists python3 || {
 }
 
 # curl file
-curl https://raw.githubusercontent.com/SantaSpeen/cp-mv-ui/master/src/main.py -o "$HOME/cp-mv-ui.py"
+curl https://raw.githubusercontent.com/SantaSpeen/cp-mv-ui/master/src/main.py -o "$HOME/.cp-mv-ui.py"
 
 # set symbolic link
-ln -s "$HOME/cp-mv-ui.py" /usr/bin/cpu
-ln -s "$HOME/cp-mv-ui.py" /usr/bin/mvu
+if user_can_sudo; then
+  sudo ln -s "$HOME/cp-mv-ui.py" /usr/bin/cpu
+  sudo ln -s "$HOME/cp-mv-ui.py" /usr/bin/mvu
+else
+  ln -s "$HOME/cp-mv-ui.py" /usr/bin/cpu
+  ln -s "$HOME/cp-mv-ui.py" /usr/bin/mvu
+fi
 
 echo Ready to use: mvu cpg
